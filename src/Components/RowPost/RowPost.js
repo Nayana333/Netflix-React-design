0908a534866c1de1b210f5e9fe5eade0d8ded69b
Nayana333 <nrnayana1@gmail.com>
2024-02-28 09:51:34 +0530

    last push

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -5,6 +5,7 @@ import YouTube from 'react-youtube';
 import {imageUrl,API_KEY} from '../../constants/constants'
 
 function RowPost(props) {
+  
   const[movies,setMovies]=useState([])
   const [urlId,setUrlId]=useState('')
   useEffect(()=>{
@@ -20,21 +21,26 @@ function RowPost(props) {
     height: '390',
     width: '100%',
     playerVars: {
-      // https://developers.google.com/youtube/player_parameters
+      //  https://developers.google.com/youtube/player_parameters
       autoplay: 0,
     },
   };
 
-  const handleMovie=(id)=>{
+  const handleMovie = (id) => {
+    axios.get(`/movie/${id}/videos?api_key=${API_KEY}`).then((response) => {
+        console.log('RESPONSE---',response.data);
+        if(response.data.results.length!==0)
+        {
+          console.log('keeeeeeeeey',response.data.results[0]);
+          setUrlId(response.data.results[0].key)
+        }else{
+          console.log('array empty')
+        }
 
-    console.log(id);
-    console.log(API_KEY);
-    axios.get(`/movie/${id}/videos?api_key=${API_KEY}`).then((response)=>{
-      console.log(response);
-      
-    }
-    )
-  }
+    })
+}
+
+
   return (
     <div className='row'>
       <h2>{props.title}</h2>
@@ -42,12 +48,12 @@ function RowPost(props) {
         {movies.map((obj)=>
           <img onClick={() => handleMovie(obj.id)} src={`${imageUrl+obj.backdrop_path}`} alt='posters' className={props.isSmall ? 'smallPoster' : 'poster'}></img> 
   
-        )}                                                                                                                                   
+        )}                                                                                                                  
      
 
       </div>
       
-     { urlId && <YouTube otps={opts } videoId={urlId.key}/>} 
+     { urlId && <YouTube opts={opts } videoId={urlId}/>} 
 
       </div>
    
